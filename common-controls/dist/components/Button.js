12f475a7555a4d797a565a6622438c251dc1f15a
pravallika2710 <pravallika.prabu@gmail.com>
2023-05-16 17:02:48 +0530

    added package.jso in dist

--- common-controls/dist/components/Button.js
+++ common-controls/dist/components/Button.js
@@ -3,11 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = void 0;
+exports.default = void 0;
 var _react = _interopRequireDefault(require("react"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var Button = function Button(props) {
-  return /*#__PURE__*/_react["default"].createElement("button", {
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const Button = props => {
+  return /*#__PURE__*/_react.default.createElement("button", {
     className: "btn btn--".concat(props.kind, " CTA"),
     "data-id": props.id,
     type: props.type,
@@ -15,7 +15,7 @@ var Button = function Button(props) {
     value: props.value,
     disabled: props.disabled,
     onClick: props.handleClick
-  }, /*#__PURE__*/_react["default"].createElement("h4", null, props.label));
+  }, /*#__PURE__*/_react.default.createElement("h4", null, props.label));
 };
 var _default = Button;
-exports["default"] = _default;
+exports.default = _default;
--- common-controls/dist/index.js
+++ common-controls/dist/index.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", {
 Object.defineProperty(exports, "Button", {
   enumerable: true,
   get: function get() {
-    return _Button["default"];
+    return _Button.default;
   }
 });
 var _Button = _interopRequireDefault(require("./components/Button"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
