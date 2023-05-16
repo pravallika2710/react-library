35e9b08943a25be39b365016309550c573946b4f
pravallika2710 <pravallika.prabu@gmail.com>
2023-05-16 12:44:09 +0530

    created library

---
+++ demo-folder/src/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import {Button} from 'common-controls'
+const App = () => {
+  return (
+    <div>
+      <Button label={"test"}/>
+    </div>
+  )
+}
+
+export default App
